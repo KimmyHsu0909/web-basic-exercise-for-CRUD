de9c392110b9bef81ecd766984b9b35f48f8e8fd
KimmyHsu0909 <msssks50@gmail.com>
2025-06-24 09:02:57 +0200

    编辑员工业务开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -127,4 +127,31 @@ public class EmployeeServiceImpl implements EmployeeService {
             employeeMapper.update(employee);
 
     }
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Override
+    public Employee getById(Long id) {
+        Employee employee = employeeMapper.getById(id);
+        employee.setPassword("****");//处理一下密码
+        return employee;
+    }
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    @Override
+    public void update(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO, employee);//对象拷贝
+
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());//设置剩下的
+
+        employeeMapper.update(employee);
+    }
 }
